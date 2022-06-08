223204463909038bd8c775ad4dd2ad0ed0107efe
twiss2k <chris.dean.1985@gmail.com>
2022-06-08 11:42:50 +0100

    Update reset_container.sh

--- sane_docker/reset_container.sh
+++ sane_docker/reset_container.sh
@@ -1,4 +1,4 @@
-docker stop webserver;
-docker rm webserver;
-docker build -t webserver .;
+docker stop webserver
+docker rm webserver
+docker build -t webserver .
 docker run --name webserver -p 80:80 -v ./content/html:/usr/share/nginx/html/ -t webserver
