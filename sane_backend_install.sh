5b10a0881e948f622be9ee5409f385ae6b2cb9f3
twiss2k <chris.dean.1985@gmail.com>
2022-06-08 01:04:37 +0100

    Update sane_backend_install.sh

--- sane_backend_install.sh
+++ sane_backend_install.sh
@@ -4,5 +4,4 @@ cd backends-RELEASE_1_0_23_FIXED2/
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var BACKENDS=kodakaio
 make
 make install
-scanimage -T
 scanimage -d kodakaio --mode gray --resolution 300 -x 215.9 -y 279.40 > MyDocScan.pgm
