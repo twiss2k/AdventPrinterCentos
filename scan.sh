e53c6e28acbb24d3fb6da04cd958659ef7c43577
twiss2k <chris.dean.1985@gmail.com>
2022-06-09 18:35:19 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,10 +4,10 @@ datetime=`date +'%Y%m%d%H%M%S'`
 storage=/storage/scans
 
 echo "Scanning..."
-scanimage -d kodakaio --mode gray --resolution 300 -x 215.9 -y 279.40 > $storage/$datetime.pgm
+scanimage -d kodakaio --mode gray --resolution 300 -x 215.9 -y 279.40 > $storage/$datetime.tiff
 sleep 20
 echo "Processing..."
-mogrify -format png $storage/*.pgm
-rm -rf $storage/*.pgm
+mogrify -format png $storage/*.tiff
+rm -rf $storage/*.tiff
 chmod 777 $storage/*
 echo "Done"
